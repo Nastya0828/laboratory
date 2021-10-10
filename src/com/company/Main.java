3a94c91916dba68efcc932e2c3c31d1ee674596a
Анастасия <anastasiia.mikhaiilova@gmail.com>
2021-10-10 22:13:10 +0500

    Домашняя работа №1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        HomeWork1 homeWork1 = new HomeWork1();
+
+        System.out.println(homeWork1.calculations(1, 2, 3, 4));
+        System.out.println(homeWork1.comparison(1, 2));
+        System.out.println(homeWork1.module(4));
+        System.out.println(homeWork1.negativeNumber(-5));
+        System.out.println(homeWork1.name("Ральф"));
+        System.out.println(homeWork1.leapYear(2021));
     }
 }
+
